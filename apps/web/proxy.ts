7deebf637864a0f0ba34b402612a4d295a8410c0
manideep1428 <saimanideep.ch12345@gmail.com>
2026-05-23 13:35:03 +0530

    feat: add development proxy configuration

---
+++ apps/web/proxy.ts
@@ -0,0 +1,15 @@
+import { authkitMiddleware } from '@workos-inc/authkit-nextjs';
+
+export default authkitMiddleware();
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    '/((?!_next/static|_next/image|favicon.ico).*)',
+  ],
+};
